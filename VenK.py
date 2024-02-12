d141984999e4feb0a583f6784a07a5d515b3af99
Imsara Samarasinghe <imsara256@gmail.com>
2024-02-12 18:55:38 +0000

    Revised and Easier

--- VenK.py
+++ VenK.py
@@ -4,47 +4,41 @@ from firedrake.adjoint import *
 import numpy as np
 continue_annotation()
 
-mesh = UnitSquareMesh(20,20)
+mesh = UnitSquareMesh(50,50)
 
-E = 20.0e6
-nu = 0.3
-lambda_ = (E*nu)/((1-nu)*(1-2*nu))
-mu = 0.5*E/(1+nu)
-rho_0 = 200.0
 
-#load
-G_val = 1.0e5# Load on Neumann boundary
-B_val = -10# Body force
-
+# function space
 V = VectorFunctionSpace(mesh,'CG',1)
-u_tr = TrialFunction(V)
+u_trial = TrialFunction(V)
 u_test = TestFunction(V)
 u = Function(V)
-g = Constant([0.0,G_val])
-b = Constant([0.0,B_val])
-N = Constant([0,1])
+B = Constant([0,0]) # body force
+G = Constant([5,0]) # traction
 
-bc1 = DirichletBC(V,Constant([0,0]),3)
-aa, bb, cc, dd, ee = 0.5*mu, 0.0, 0.0, mu, -1.5*mu
+# material
+E, nu = 10, 0.3
+lambda_, mu = E*nu/((1-nu)*(1-2*nu)), 0.5*E/(1+nu)
 
-# Weak form
-I = Identity(2)
-F = I + grad(u)
+# params
+F = Identity(2)+grad(u)
 C = F.T*F
 J = det(F)
+Ic = tr(C)
 
-n = dot(cofac(F),N)
-surface_def = sqrt(inner(n,n))
-psi = (aa*inner(F,F)+ee-dd*ln(J))*dx-rho_0*J*dot(b,u)*dx+surface_def*inner(g,u)*ds(1)
 
-#solver
-Form = derivative(psi,u,u_test)
-Jac = derivative(Form,u,u_tr)
+# dirichlet
+bc1 = DirichletBC(V,Constant([0,0]),3)
+bc2 = DirichletBC(V,Constant([0,0]),4)
 
-problem = NonlinearVariationalProblem(Form,u,bc1,Jac)
-solver = NonlinearVariationalSolver(problem)
-solver.solve()
+# SED
+psi = (mu/2)*(Ic-3)-mu*ln(J)+(lambda_/2)*(ln(J))**2
+# Potential Energy
+PI = psi*dx - dot(B,u)*dx - dot(G,u)*ds(2)
 
+F = derivative(PI,u,u_test)
+J = derivative(F,u,u_trial)
+
+solve(F == 0, u, bcs=[bc1,bc2], J=J)
 
 # --- Plot ---
 fig, axes = plt.subplots()
@@ -52,5 +46,3 @@ collection = tripcolor(u, axes=axes, cmap='Greys')
 fig.colorbar(collection);
 plt.savefig("Optimised Beam.png")
 plt.show()
-
-
